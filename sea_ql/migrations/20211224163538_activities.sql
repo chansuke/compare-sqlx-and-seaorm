ce83b1ec123b84c437f4ab3f5117a4f1c15243a1
chansuke <moonset20@gmail.com>
2021-12-25 14:53:14 +0900

    added: connect options

--- sea_ql/migrations/20211224163538_activities.sql
+++ sea_ql/migrations/20211224163538_activities.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 
-CREATE TABLE IF NOT EXISTS activities {
+CREATE TABLE IF NOT EXISTS activities (
   "uuid" character varying(50) NOT NULL
   ,"name" text NOT NULL
   ,"category_id" text NOT NULL
@@ -8,4 +8,4 @@ CREATE TABLE IF NOT EXISTS activities {
   ,"updated_at" timestamp with time zone NOT NULL
   ,"deleted_at" timestamp with time zone NOT NULL
   ,PRIMARY KEY ("uuid")
-}
+)
---
+++ sea_ql/src/main.rs
@@ -0,0 +1,21 @@
+use sea_orm::prelude::*;
+use sea_orm::{ConnectOptions, Database};
+use std::env;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<(), anyhow::Error> {
+    tracing_subscriber::fmt().init();
+
+    // connect options
+    let mut opt = ConnectOptions::new(env!("DATABASE_URL").to_owned());
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .sqlx_logging(true);
+
+    let db = Database::connect(opt).await?;
+
+    Ok(())
+}
